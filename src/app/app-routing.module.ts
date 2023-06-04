b5f3e338ee115b53686627ee079a532601424787
Agustin Contreras <35529189+aguscontreras@users.noreply.github.com>
2023-06-04 16:12:01 -0300

    error route config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
   },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
