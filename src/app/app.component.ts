ead6635d2b81251f28869abf187b4e069d75d7de
Agustin Contreras <35529189+aguscontreras@users.noreply.github.com>
2023-06-04 16:12:10 -0300

    app component init db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from './services/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'cerramos-numeros-angular';
+export class AppComponent implements OnInit {
+  constructor(private storageService: StorageService, private router: Router) {}
+
+  ngOnInit() {
+    this.initDB();
+  }
+
+  private async initDB() {
+    try {
+      await this.storageService.initDb();
+    } catch (error) {
+      this.router.navigate(['error']);
+    }
+  }
 }
