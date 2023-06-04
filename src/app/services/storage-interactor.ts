d873625b8178234060ba2130606107a7d9531099
Agustin Contreras <35529189+aguscontreras@users.noreply.github.com>
2023-06-04 19:48:12 -0300

    add storage interactor class

---
+++ src/app/services/storage-interactor.ts
@@ -0,0 +1,42 @@
+import { StorageService } from './storage.service';
+import { LocalDBSchema } from '../models';
+import { StoreNames } from 'idb';
+
+export class StorageInteractor<N extends StoreNames<LocalDBSchema>> {
+  constructor(private storageService: StorageService, private storeKey: N) {}
+
+  async getAll() {
+    await this.storageService.awaitForDb();
+    const allItems = this.storageService.db?.getAll(this.storeKey);
+
+    return allItems;
+  }
+
+  async get(id: string) {
+    await this.storageService.awaitForDb();
+    const item = await this.storageService.db?.get(this.storeKey, id);
+
+    return item;
+  }
+
+  async add(
+    item: LocalDBSchema[N]['value']
+  ): Promise<LocalDBSchema[N]['value']> {
+    await this.storageService.awaitForDb();
+    await this.storageService.db?.add(this.storeKey, item);
+
+    return item;
+  }
+
+  async update(id: string, item: LocalDBSchema[N]['value']) {
+    await this.storageService.awaitForDb();
+    await this.storageService.db?.put(this.storeKey, item, id);
+
+    return item;
+  }
+
+  async delete(id: string) {
+    await this.storageService.awaitForDb();
+    await this.storageService.db?.delete(this.storeKey, id);
+  }
+}
