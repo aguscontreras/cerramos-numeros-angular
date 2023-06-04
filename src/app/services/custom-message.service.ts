426480a7b6ee72881950fb9196d8c32d1a2a9902
Agustin Contreras <35529189+aguscontreras@users.noreply.github.com>
2023-06-04 20:54:31 -0300

    add messages

---
+++ src/app/services/custom-message.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { MessageService } from 'primeng/api';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomMessageService {
+  constructor(private messageService: MessageService) {}
+
+  showSuccess(summary: string, detail?: string) {
+    return this.messageService.add({
+      summary,
+      detail,
+      severity: 'success',
+      icon: 'pi pi-check-circle',
+    });
+  }
+
+  showError(summary: string, detail?: string) {
+    return this.messageService.add({
+      summary,
+      detail,
+      severity: 'error',
+      icon: 'pi pi-times-circle',
+    });
+  }
+}
