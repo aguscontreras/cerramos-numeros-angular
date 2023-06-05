075c1e3351d9d0305b249db2bac542a43b0ff6c9
Agustin Contreras <35529189+aguscontreras@users.noreply.github.com>
2023-06-05 17:15:23 -0300

    integrate state preview

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { MemberStateService } from '../../services/member-state.service';
+import { CategoryStateService } from '../../services/category-state.service';
+import { ExpenseStateService } from '../../services/expense-state.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  members$ = this.membersStateService.allItems$;
+
+  categories$ = this.categoryStateService.allItems$;
+
+  totalAmount$ = this.expenseStateService.totalAmount$;
+
+  constructor(
+    private membersStateService: MemberStateService,
+    private categoryStateService: CategoryStateService,
+    private expenseStateService: ExpenseStateService
+  ) {}
+}
